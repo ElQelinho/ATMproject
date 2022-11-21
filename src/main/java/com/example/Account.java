254f762a8509f0e92440048a4fced0ec0656cc0a
ElQelinho <elqelinho@gmail.com>
2022-11-21 20:54:30 +0300

    change float type to bigdecimal

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.math.BigDecimal;
+
 public class Account {
 
     Account(String firstUserName,String secondUserName, int cardNum, byte pin) {
@@ -7,7 +9,7 @@ public class Account {
         this.secondUserName = secondUserName;
         this.cardNum = cardNum;
         this.pin = pin;
-        balance = 0;
+        balance = BigDecimal.ZERO;
     }
 
     String firstUserName;
@@ -17,11 +19,6 @@ public class Account {
     int cardNum;
 
     byte pin;
-    // FIXME остаток по счету следует вести в значении такого, типа, который не будет иметь погрешностей
-    //  при выполнении над ним арифметических операций. Тип float не подойдет, потому, что 5.3 + 4.7 != 10.0 из-за
-    //  погрешности представления чисел с плавающей точкой в двоичной системе исчисления.
-    //  Можно использовать long и вести сумму в копейках, или BigDecimal.
 
-    float balance;
-
+    BigDecimal balance;
 }
